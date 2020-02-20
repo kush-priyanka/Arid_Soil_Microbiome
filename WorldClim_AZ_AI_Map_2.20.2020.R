102b25ebe8d8f7efe9672bd5bb3d3d696ef4f771
pkush003 <pkushwaha@email.arizona.edu>
2020-02-20 16:25:11 -0700

    Update AI site map

--- WorldClim_AZ_AI_Map_2.20.2020.R
+++ WorldClim_AZ_AI_Map_2.20.2020.R
@@ -4,14 +4,20 @@ install.packages("sp")
 ##load libraries
 library(raster)
 library(sp)
-#Import Tiff file containing Global Aridity Index (AI)
-##Make sure to use the location of your file
+#Download data files for global Aridity and PET data on your computer from the weblink: 
+#https://figshare.com/articles/Global_Aridity_Index_and_Potential_Evapotranspiration_ET0_Climate_Database_v2/7504448/3
+#Extract all the files on your computer: ET_annual, ET_monthly, AI_annual
+#You need .tif files to extract data
+
+#Now import .tif file containing Global Aridity Index (AI) data
+##Make sure to use the correct location of your file
 ai_et_0<-raster(x="C:/Users/pree/Documents/R_worldClimData/global-ai_et0/ai_et0/ai_et0.tif")
 #Multiply AI values by 0.0001 for actual data
 ai_values<-ai_et_0*0.0001
 #Plot global AI
 plot(ai_values)
-#Create a variable with outline of AZ boundaries 
+
+#Create a variable with longitude and latitude outline of AZ boundaries 
 #latitude and longitude valuesspecific for AZ
 az <- as(extent(-114.866667, -109,31.333333, 37), 'SpatialPolygons')
 crs(az) <- "+proj=longlat +datum=WGS84 +no_defs"
